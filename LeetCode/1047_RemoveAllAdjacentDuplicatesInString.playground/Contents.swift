1ba5cc8d563ad373281dd53660224f96dfdceb30
Xushnidjon.Keldiboev <xushnidjon.keldiboev@itechart-group.com>
2022-08-26 14:44:14 +0500

    1047 Solved

--- LeetCode/1047_RemoveAllAdjacentDuplicatesInString.playground/Contents.swift
+++ LeetCode/1047_RemoveAllAdjacentDuplicatesInString.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+class Solution {
+    func removeDuplicates(_ s: String) -> String {
+        var array = [Character]()
+        
+        s.forEach {
+            if array.last == $0 {
+                array.removeLast()
+            } else {
+                array.append($0)
+            }
+        }
+        
+        return String(array)
+    }
+}
